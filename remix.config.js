da201a395528d3e08e0a784c35bba7d42645f1cc
Ana Cecilia <ana.ceci7373@gmail.com>
2025-04-06 00:47:55 -0300

    fix: ajusta configurações do Remix com Vite

--- remix.config.js
+++ remix.config.js
@@ -2,5 +2,12 @@
 module.exports = {
     ignoredRouteFiles: ["**/.*"],
     tailwind: true,
-    serverModuleFormat: "cjs"
+    serverModuleFormat: "esm",
+    future: {
+      v3_fetcherPersist: true,
+      v3_relativeSplatPath: true,
+      v3_throwAbortReason: true,
+      v3_singleFetch: true,
+      v3_lazyRouteDiscovery: true
+    }
   };
