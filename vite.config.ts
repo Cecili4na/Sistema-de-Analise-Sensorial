913376677a030ae6fcf0e22c219f36fc6caf0008
Ana Cecilia <ana.ceci7373@gmail.com>
2025-04-08 09:46:20 -0300

    fix: tentativa 4

--- vite.config.ts
+++ vite.config.ts
@@ -12,13 +12,5 @@ export default defineConfig({
   plugins: [remix(), tsconfigPaths()],
   ssr: {
     noExternal: ["@prisma/client", "prisma"]
-  },
-  optimizeDeps: {
-    exclude: ['rollup']
-  },
-  build: {
-    rollupOptions: {
-      external: ['rollup']
-    }
   }
 });
